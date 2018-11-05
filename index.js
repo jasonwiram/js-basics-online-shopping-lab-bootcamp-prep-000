3350671c463eef85d1815ba20f694d0f910da414
Jason Wiram <jwiram@gmail.com>
2018-11-05 14:59:10 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -48,14 +48,11 @@ function total() {
 function removeFromCart(item) {
   for (var i = 0; i < cart.length; i++){
     if (item === cart[i].itemName) {
-      cart.splice(i, 1); //removes 1 item at index i
+      cart.splice(i, 1);
       return cart;
     }
   }
-
-  if (item != cart[item]) {
-    return "That item is not in your cart."
-  }
+  return "That item is not in your cart."
 }
 
 function placeOrder(cardNumber) {
