d6b8cd95cf065471327217d688b59ff84e2d2c88
Jason Wiram <jwiram@gmail.com>
2018-11-05 14:58:51 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -10,19 +10,52 @@ function setCart(c) {
 }
 
 function addToCart(item) {
- // write your code here
+  var newItem = {
+    "itemName":item,
+    "itemPrice": Math.floor(Math.random() * 100 + 1)
+  }
+  cart.push(newItem)
+  return `${item} has been added to your cart.`
 }
 
 function viewCart() {
-  // write your code here
+  if (cart.length === 0) {
+    return "Your shopping cart is empty."
+  } else {
+    var items = [];
+    for (var i = 0; i < cart.length; i++) {
+      items.push(`${cart[i].itemName} at $${cart[i].itemPrice}`);
+    }
+  }
+  
+  if (cart.length === 1) {
+    return `In your cart, you have ${items}.`;
+  } else {
+    var lastItem = items.pop();
+    var itemList = items.join(', ') + `, and ${lastItem}`;
+    return `In your cart, you have ${itemList}.`
+  }
 }
 
 function total() {
-  // write your code here
+  var total = 0;
+  for (var i = 0; i < cart.length; i++) {
+    total += cart[i].itemPrice;
+  }
+  return total;
 }
 
 function removeFromCart(item) {
-  // write your code here
+  for (var i = 0; i < cart.length; i++){
+    if (item === cart[i].itemName) {
+      cart.splice(i, 1); //removes 1 item at index i
+      return cart;
+    }
+  }
+
+  if (item != cart[item]) {
+    return "That item is not in your cart."
+  }
 }
 
 function placeOrder(cardNumber) {
