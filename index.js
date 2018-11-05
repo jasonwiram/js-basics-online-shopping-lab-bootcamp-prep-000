7117031fdb6ea5fa2b02c151cb76ac7e71d84e2e
Jason Wiram <jwiram@gmail.com>
2018-11-05 15:04:48 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -57,6 +57,6 @@ function removeFromCart(item) {
 
 function placeOrder(cardNumber) {
   if (cardNumber === undefined) {
-    console.log("Sorry, we don't have a credit card on file for you.");
+    return "Sorry, we don't have a credit card on file for you."
   }
 }
